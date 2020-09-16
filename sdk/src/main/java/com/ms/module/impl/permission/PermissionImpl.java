bb8c99851c980e8b44f2e24175a00e98590f931f
m_maohuawei <maohuawei@bdlbsc.com>
2020-09-16 18:06:48 +0800

    更新回调接口类

--- sdk/src/main/java/com/ms/module/impl/permission/PermissionImpl.java
+++ sdk/src/main/java/com/ms/module/impl/permission/PermissionImpl.java
@@ -5,12 +5,13 @@ import androidx.fragment.app.FragmentActivity;
 
 import com.ms.module.supers.inter.common.ICallBack;
 import com.ms.module.supers.inter.permission.IPermission;
+import com.ms.module.supers.inter.permission.PermissionCallBack;
 
 public class PermissionImpl implements IPermission {
 
 
     @Override
-    public void request(FragmentActivity fragmentActivity, final ICallBack iCallBack, String... strings) {
+    public void request(FragmentActivity fragmentActivity, final PermissionCallBack iCallBack, String... strings) {
 
         PermissionUtilsImpl.requestPermission(fragmentActivity, new PermissionUtilsImpl.CallBack() {
             @Override
